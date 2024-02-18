0898ad57115e2a500d5f7593fa39dbc97e10eb80
mingeunoh6 <mingenuoh6@gmail.com>
2024-02-18 23:39:14 +0900

    model viewer test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,19 @@
 </script>
 
 <h1>Page</h1>
+<model-viewer
+	src="models/ChairWings.glb"
+	ar
+	ar-modes="webxr scene-viewer quick-look"
+	camera-controls
+	tone-mapping="commerce"
+	shadow-intensity="1"
+>
+	<div class="progress-bar hide" slot="progress-bar">
+		<div class="update-bar"></div>
+	</div>
+	<button slot="ar-button" id="ar-button"> View in your space </button>
+</model-viewer>
 
 <style>
 	h1 {
